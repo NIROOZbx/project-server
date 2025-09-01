ff7f06c488d3d07c9a2efb53c19199ca221ae33d
NIROOZbx <nyroozashraf@gmail.com>
2025-09-01 14:12:07 +0530

    standardize image format

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+
+// This line is changed to serve images from the "public" folder
+const middlewares = jsonServer.defaults({ static: 'public' });
 
 const port = process.env.PORT || 3001;
 
